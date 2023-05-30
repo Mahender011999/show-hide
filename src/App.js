c1c66f981f18813d2b841bab18e1d972e48758b5
mahender011999 <mahender011999@gmail.com>
2023-05-30 16:29:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React,{useState} from 'react'
+import './App.css'
 function App() {
+  const [showData,setShowData] = useState(false)
+  const showHiden = () =>{
+    setShowData(!showData)
+  } 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <center>
+      <button className='button' onClick={showHiden}>{showData ? 'Hide':'Show'}</button>
+      <div>
+     {showData ?  ( <p className='para'>Est nulla magna nulla deserunt nisi.<br/> 
+     Officia do adipisicing sint sit enim elit excepteur <br/>
+     anim incididunt. Sunt sit exercitation velit laborum culpa <br/>
+     sunt ipsum quis duis velit minim aute incididunt cillum.<br/>
+      Consequat velit adipisicing irure nulla elit cupidatat aute do.<br/>
+       Pariatur sit occaecat ullamco ipsum culpa.</p>):(
+        <h1>Data is Hidden</h1>
+       )}
+      </div>
+      </center>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
